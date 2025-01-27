5caaf1174e9531bb1a6e0d7fd3aa2a97ba992e49
Dennies Ouma <144390920+Oumadenis1779@users.noreply.github.com>
2025-01-28 01:17:17 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,7 @@
-"""A simple Python script to demonstrate function usage."""
-
 def hello():
-    """Print 'hello world'."""
     print("hello world")
 
 def come():
-    """Print 'come here'."""
     print("come here")
 
 # Call the hello function
